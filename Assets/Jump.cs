2340d6a7df5c34e260912f853d1e90a9bfb41b88
Dennyie <rpereiracordeiro9@gmail.com>
2022-08-26 15:25:39 -0300

    Initial commit

---
+++ Assets/Jump.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public GameObject Nose;
+    private Rigidbody2D rigidbody2d;
+    public float jump;
+    public float tempo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = Nose.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody2d.velocity = Vector2.up * jump;
+        }
+            if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Pulotempo());
+        }
+    }
+    // Private para que ele fique somente nesse objeto
+    // IEnumerator para criar uma coroutine 
+    private IEnumerator Pulotempo()
+    {
+        rigidbody2d.bodyType = RigidbodyType2D.Kinematic;
+        // está fazendo o ridigbody ficar no estado Kinematic 
+        yield return new WaitForSeconds(tempo);
+        // yield return está retornando para função o valor de tempo
+        rigidbody2d.bodyType = RigidbodyType2D.Dynamic;
+        // está fazendo o rigidbody voltar a ser Dynamic
+    }
+
+}
