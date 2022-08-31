3341f9a8b920b8d565cd24de54635ef919cae124
Dennyie <rpereiracordeiro9@gmail.com>
2022-08-31 14:00:23 -0300

    Rigidbody
    Mudei a forma com que o personagem usava para descer e subir o bico, Hartax sugeriu usar a força do Y negativa para fazer o bico descer. Também fiz algumas mudanças em questão de organização.

--- Assets/Jump.cs
+++ Assets/Jump.cs
@@ -7,7 +7,10 @@ public class Jump : MonoBehaviour
     public GameObject Flappy;
     private Rigidbody2D rigidbody2d;
     public float jump;
-    public float tempo;
+    public float time;
+    public float timeRoutate;
+    public Transform flappydown;
+    public float timecount;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,23 +22,37 @@ public class Jump : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rigidbody2d.velocity = Vector2.up * jump;
+            Jumpflappy();
+            Scaleflappy();
+            StartCoroutine(JumptimeRoutine());
         }
-            if (Input.GetKeyDown(KeyCode.Space))
+        if (rigidbody2d.velocity.y < 0)
         {
-            StartCoroutine(Pulotempo());
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, flappydown.rotation, timecount * Time.deltaTime);
         }
     }
     // Private para que ele fique somente nesse objeto
     // IEnumerator para criar uma coroutine 
-    private IEnumerator Pulotempo()
+    private IEnumerator JumptimeRoutine()
     {
         rigidbody2d.bodyType = RigidbodyType2D.Kinematic;
+        this.transform.rotation = Quaternion.Euler(0, 0, 30);
         // está fazendo o ridigbody ficar no estado Kinematic 
-        yield return new WaitForSeconds(tempo);
+        yield return new WaitForSeconds(time);
         // yield return está retornando para função o valor de tempo
+      //  StartCoroutine(RotationRoutine());
         rigidbody2d.bodyType = RigidbodyType2D.Dynamic;
         // está fazendo o rigidbody voltar a ser Dynamic
+    }
+
+    private void Jumpflappy()
+    {
+        rigidbody2d.velocity = Vector2.up * jump;
+    }
+
+    private void Scaleflappy()
+    {
+        this.transform.rotation = Quaternion.Euler(0, 0, 30);
     }
 
 }
