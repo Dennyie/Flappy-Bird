6d25c877d07f3636798e88ac9b310395eccecb6d
Dennyie <rpereiracordeiro9@gmail.com>
2022-09-01 09:07:53 -0300

    Coroutine
    Fazendo Corotina pra testar fazer o bico do flappy ficar para cima e só descer após um certo tempo.

--- Assets/Jump.cs
+++ Assets/Jump.cs
@@ -8,6 +8,7 @@ public class Jump : MonoBehaviour
     private Rigidbody2D rigidbody2d;
     public float jump;
     public float time;
+    public float timeup;
     public float timeRoutate;
     public Transform flappydown;
     public float timecount;
@@ -23,24 +24,24 @@ public class Jump : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Jumpflappy();
-            Scaleflappy();
+           // Scaleflappy();
             StartCoroutine(JumptimeRoutine());
+            StartCoroutine(UpdownRoutine());
         }
-        if (rigidbody2d.velocity.y < 0)
+     /*   if (rigidbody2d.velocity.y < 0)
         {
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation, flappydown.rotation, timecount * Time.deltaTime);
-        }
+        } */
     }
     // Private para que ele fique somente nesse objeto
     // IEnumerator para criar uma coroutine 
     private IEnumerator JumptimeRoutine()
     {
         rigidbody2d.bodyType = RigidbodyType2D.Kinematic;
-        this.transform.rotation = Quaternion.Euler(0, 0, 30);
         // está fazendo o ridigbody ficar no estado Kinematic 
         yield return new WaitForSeconds(time);
         // yield return está retornando para função o valor de tempo
-      //  StartCoroutine(RotationRoutine());
+        //  StartCoroutine(RotationRoutine());
         rigidbody2d.bodyType = RigidbodyType2D.Dynamic;
         // está fazendo o rigidbody voltar a ser Dynamic
     }
@@ -55,4 +56,14 @@ public class Jump : MonoBehaviour
         this.transform.rotation = Quaternion.Euler(0, 0, 30);
     }
 
+    private IEnumerator UpdownRoutine()
+    {
+        this.transform.rotation = Quaternion.Euler(0, 0, 30);
+        yield return new WaitForSeconds(timeup);
+        Debug.Log("velocity " + rigidbody2d.velocity.y);
+        if (rigidbody2d.velocity.y < 0)
+        {
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, flappydown.rotation, timecount * Time.deltaTime);
+        }
+    }
 }
